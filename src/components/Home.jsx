60d90e7adb462cc74c5821474372996e4f8c1156
edd-ie <ogendoeddie@gmail.com>
2023-06-07 19:05:42 +0300

    routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>Landing</div>
+    <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/flappy">Flappy</Link></li>
+        <li><Link to="/2048">2048</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+    </ul>
   )
 }
